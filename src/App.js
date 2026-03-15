fe110c95593dce16d8ea68fe91e34976aca3285f
Scott <190669978+Krimrook@users.noreply.github.com>
2026-03-15 14:55:01 +0000

    Test update of homepage text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Scott's Meal Planner - I'm Building This!
+          Scott's Meal Planner - LIVE ON THE INTERNET! 🚀
         </p>
         <a
           className="App-link"
